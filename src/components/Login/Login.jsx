651d8c001163f28662401175d1e35185faef19e2
rohan future devoloper <xrohaislam373@gmail.com>
2023-06-26 02:18:16 +0600

    display name Add

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
+import app from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
 
+const auth = getAuth(app);
 const Login = () => {
-    const [error, setError] = useState('');
-    const [success, setSucess] = useState('');
+  const [error, setError] = useState("");
+  const [success, setSucess] = useState("");
+  const emailRef = useRef();
+
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -10,21 +16,44 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
-    setError('')
-    setSucess('')
+    setError("");
+    setSucess("");
     event.target.reset();
-    if(!/(?=.*[A-Z])/.test(password)){
-        setError('add at least one upper case');
-        return
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("add at least one upper case");
+      return;
+    } else if (!/(?=.*[0-9])/.test(password)) {
+      setError("please add one digit");
+      return;
+    } else if (password.length < 6) {
+      setError("must be use six character");
     }
-    else if(!/(?=.*[0-9])/.test(password)){
-        setError('please add one digit');
-        return ;
-    }
-    else if(password.length <6){
-        setError('must be use six character')
-    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggeduser = result.user;
+        setSucess('User Logged In SuccessFully');
+        console.log(loggeduser)
+        setError("");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   };
+  const handleResetPassword = event =>{
+  const email = (emailRef.current.value);
+  if(!email){
+    alert('please provide your email address to reset password')
+    return
+  }
+  sendPasswordResetEmail(auth, email)
+  .then(() =>{
+    alert('please cheak your email')
+  })
+  .catch(error =>{
+    console.log(error);
+    setError(error.message)
+  })
+  }
   return (
     <div className="w-25 mx-auto">
       <h3>This is Login Page</h3>
@@ -34,6 +63,7 @@ const Login = () => {
           <input
             type="email"
             name="email"
+            ref={emailRef}
             required
             className="form-control"
             id="email"
@@ -56,9 +86,11 @@ const Login = () => {
         <button type="submit" className="btn btn-primary">
           Login
         </button>
+        <p><small>Have an account ? Please <Link to="/register">Register</Link></small></p>
       </form>
-        <p className="text-danger">{error}</p>
-        <p className="text-success">{success}</p>
+      <p><small>Forget Password ? please<button onClick={handleResetPassword} className="btn btn-link">Reset Password</button></small></p>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,56 +1,87 @@
 import React, { useState } from "react";
 import app from "../../firebase/firebase.config";
-import{createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
+import { Link } from "react-router-dom";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 const Register = () => {
-  const [error, setError] = useState('');
-  const [sucess, setSucess] = useState('');
+  const [error, setError] = useState("");
+  const [sucess, setSucess] = useState("");
   const handleEmailChange = (event) => {
     // setEmail(event.target.value);
   };
-  const handlePasswordBlur = (event) => {
-  };
+  const handlePasswordBlur = (event) => {};
 
-  const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setSucess('')
-    setError('');
-    const email = event.target.email.value
-    const Password = event.target.Password.value
-   console.log(email, Password)
-   if(!/(?=.*[0-9])/.test(Password)){
-    setError('pleace add atleast one number');
-    return;
-   }
-   else if(!/(?=.*[!@#$%^&*])/.test(Password)){
-    setError('please add atleast one speacial character');
-    return ;
-   }
-   else if (Password.length<6){
-    setError('please add a six character');
-    return ;
-   }
-   createUserWithEmailAndPassword(auth, email, Password)
-   .then( result =>{
-    const loggedIn = result.user;
-    setError('');
-    event.target.reset();
-    setSucess('user has created Successfully')
-    console.log(loggedIn)
-   })
-   .catch(error =>{
-    console.log(error.message)
-    setError(error.message)
-   })
-  }
+    setSucess("");
+    setError("");
+    const email = event.target.email.value;
+    const Password = event.target.Password.value;
+    const name = event.target.name.value;
+    console.log(name, email, Password);
+    if (!/(?=.*[0-9])/.test(Password)) {
+      setError("pleace add atleast one number");
+      return;
+    } else if (!/(?=.*[!@#$%^&*])/.test(Password)) {
+      setError("please add atleast one speacial character");
+      return;
+    } else if (Password.length < 6) {
+      setError("please add a six character");
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, Password)
+      .then((result) => {
+        const loggedIn = result.user;
+        setError("");
+        event.target.reset();
+        setSucess("user has created Successfully");
+        sendVerificaionEmail(result.user);
+        updateUserData(result.user , name)
+        console.log(loggedIn);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+      });
+  };
+  const sendVerificaionEmail = (user) => {
+    sendEmailVerification(user).then((result) => {
+      console.log(result);
+      alert("please verify your email address");
+    });
+  };
+  const updateUserData = (user, name) => {
+    updateProfile(user, {
+      displayName: name,
+    })
+      .then(() => {})
+      .catch((error) => {
+        setError(error.message  );
+      });
+  };
 
   return (
     <div className="w-50 mx-auto">
       <h4>Pleace Register</h4>
       <form onSubmit={handleSubmit}>
-        <input className="mb-4 w-50 rounded ps-2"
+        <input
+          className="mb-4 w-50 rounded ps-2"
+          type="terxt"
+          name="name"
+          id="name"
+          placeholder="Your Name"
+          required
+        />
+        <br />
+        <input
+          className="mb-4 w-50 rounded ps-2"
           onChange={handleEmailChange}
           type="email"
           name="email"
@@ -59,7 +90,8 @@ const Register = () => {
           required
         />
         <br />
-        <input className="mb-4 w-50 rounded ps-2"
+        <input
+          className="mb-4 w-50 rounded ps-2"
           onBlur={handlePasswordBlur}
           type="Password"
           name="Password"
@@ -68,8 +100,13 @@ const Register = () => {
           required
         />
         <br />
-      <p className="text-danger">{error}</p>
+        <p className="text-danger">{error}</p>
         <input className="btn btn-primary" type="Submit" value="register" />
+        <p>
+          <small>
+            Already hava an Account ? Please <Link to="/login">Login</Link>
+          </small>
+        </p>
       </form>
       <p className="text-success">{sucess}</p>
     </div>
